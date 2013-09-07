b8639e253aff95a05fe85af055ceab378e61d429
Victor Nicollet <victor@nicollet.net>
2013-09-07 12:16:45 +0200

    Mock object equality comparison

--- SocialToolBox.Core.Mocks/Database/Serialization/MockAccount.cs
+++ SocialToolBox.Core.Mocks/Database/Serialization/MockAccount.cs
@@ -3,16 +3,28 @@
 namespace SocialToolBox.Core.Mocks.Database.Serialization
 {
     [Serializable]
-    public class MockAccount
+    public class MockAccount : IEquatable<MockAccount>
     {
         [Serializable]
-        public class HashedPassword
+        public class HashedPassword : IEquatable<HashedPassword>
         {
             public int BcryptIterationCount;
             public string Hash;
+
+            public bool Equals(HashedPassword other)
+            {
+                return other.BcryptIterationCount == BcryptIterationCount
+                       && other.Hash == Hash;
+            }
         }
 
         public string Name;
         public HashedPassword Password;
+
+        public bool Equals(MockAccount other)
+        {
+            return other.Name == Name
+                   && Password.Equals(other.Password);
+        }
     }
 }
