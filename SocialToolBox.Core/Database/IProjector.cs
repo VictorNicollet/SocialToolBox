3fcf5f7d8a1d476c14c10b77c2e1530769f5ef29
Victor Nicollet <victor@nicollet.net>
2013-09-07 15:34:52 +0200

    Projectors

---
+++ SocialToolBox.Core/Database/IProjector.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+namespace SocialToolBox.Core.Database
+{
+    /// <summary>
+    /// Receives events for processing, commits results for persistence.
+    /// </summary>
+    public interface IProjector<in T>
+    {
+        /// <summary>
+        /// Given the current state of the projector, a commit is recommended.
+        /// </summary>
+        bool CommitRecommended { get; }
+
+        /// <summary>
+        /// Process one event. This is allowed to have side-effects,
+        /// as long as those side-effecs are only made permanent 
+        /// after <see cref="Commit()"/> is called.
+        /// </summary>
+        void ProcessEvent(T ev);
+
+        /// <summary>
+        /// Make all operations performed so far permanent.
+        /// </summary>
+        Task Commit();
+    }
+}
