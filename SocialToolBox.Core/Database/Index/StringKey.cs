5ab135c3ca5d3196f827327bec9f8ca7aa96669e
Victor Nicollet <victor@nicollet.net>
2013-09-13 11:50:47 +0200

    String keys

---
+++ SocialToolBox.Core/Database/Index/StringKey.cs
@@ -0,0 +1,18 @@
+﻿namespace SocialToolBox.Core.Database.Index
+{
+    /// <summary>
+    /// A one-column, 255-character string key. Provided as a helpful utility class.
+    /// </summary>
+    [IndexKey]
+    public class StringKey
+    {
+        [IndexField(0)] public readonly string Key;
+
+        public StringKey(string key) { Key = key; }
+
+        public override string ToString()
+        {
+            return Key;
+        }
+    }
+}
