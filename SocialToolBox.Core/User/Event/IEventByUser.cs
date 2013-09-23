eee4dc367ed47ca4cde6cf7d8ce137762fb9ae7f
Victor Nicollet <victor@nicollet.net>
2013-09-23 13:55:34 +0200

    CRM : contact events

---
+++ SocialToolBox.Core/User/Event/IEventByUser.cs
@@ -0,0 +1,13 @@
+﻿using SocialToolBox.Core.Database;
+
+namespace SocialToolBox.Core.User.Event
+{
+    /// <summary>
+    /// An event which was performed by an user (called
+    /// the author).
+    /// </summary>
+    public interface IEventByUser
+    {
+        Id AuthorId { get; }
+    }
+}
---
+++ SocialToolBox.Crm/Contact/Event/IContactEvent.cs
@@ -0,0 +1,12 @@
+﻿using SocialToolBox.Core.Database.Event;
+using SocialToolBox.Core.User.Event;
+
+namespace SocialToolBox.Crm.Contact.Event
+{
+    /// <summary>
+    /// An event related to a contact, as part of the CRM module set.
+    /// </summary>
+    public interface IContactEvent : IEventWithId, IEventWithTime, IEventByUser
+    {
+    }
+}
