e572df750fa7d202279a933e8e73051f797f019e
Victor Nicollet <victor@nicollet.net>
2013-09-23 14:23:07 +0200

    Contact module and extensions

---
+++ SocialToolBox.Crm/Contact/ContactModule.cs
@@ -0,0 +1,28 @@
+﻿using SocialToolBox.Core.Database;
+
+namespace SocialToolBox.Crm.Contact
+{
+    public class ContactModule
+    {
+        /// <summary>
+        /// The name of the <see cref="Stream"/>.
+        /// </summary>
+        public const string StreamName = "SocialToolBox.Crm.Contact.ContactModule.Stream";
+
+        /// <summary>
+        /// The database driver used by this module.
+        /// </summary>
+        public readonly IDatabaseDriver Driver;
+
+        /// <summary>
+        /// The stream to which all contact-related events should be appended to.
+        /// </summary>
+        public readonly IEventStream Stream;
+
+        public ContactModule(IDatabaseDriver driver)
+        {
+            Driver = driver;
+            Stream = driver.GetEventStream(StreamName, true);
+        }
+    }
+}
---
+++ SocialToolBox.Crm/Contact/ContactModuleEntityExtensions.cs
@@ -0,0 +1,22 @@
+﻿using SocialToolBox.Core.Entity;
+using SocialToolBox.Crm.Contact.Projection;
+
+namespace SocialToolBox.Crm.Contact
+{
+    /// <summary>
+    /// Extensions involved in making the contact module connect to
+    /// the entity module.
+    /// </summary>
+    public static class ContactModuleEntityExtensions
+    {
+        /// <summary>
+        /// Register all contacts as entities : these will appear as entity
+        /// pages, in entity searches, and so on.
+        /// </summary>
+        public static void RegisterContactsAsEntities(this ContactModule cmodule, EntityModule emodule)
+        {
+            emodule.AddEventStream(cmodule.Stream);
+            ContactAsEntityPage.ExtendVisitor(emodule.PageEventVisitor);
+        }
+    }
+}
