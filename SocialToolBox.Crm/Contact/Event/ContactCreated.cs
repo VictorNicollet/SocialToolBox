32a735d49337cba151591e94646112c0382e6556
Victor Nicollet <victor@nicollet.net>
2013-09-23 14:02:45 +0200

    Contact creation and entity page

---
+++ SocialToolBox.Crm/Contact/Event/ContactCreated.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using SocialToolBox.Core.Database;
+using SocialToolBox.Core.Database.Serialization;
+using SocialToolBox.Core.Entity.Event;
+
+namespace SocialToolBox.Crm.Contact.Event
+{
+    /// <summary>
+    /// A new contact is created. It does not yet have a name or e-mail.
+    /// </summary>
+    [Persist("SocialToolBox.Crm.Contact.Event.ContactCreated")]
+    public class ContactCreated : IContactEvent, IEntityPageEvent
+    {
+        /// <summary>
+        /// The identifier of the created contact.
+        /// </summary>
+        [PersistMember(0)]
+        public Id Id { get; private set; }
+
+        /// <summary>
+        /// The time when the contact was created.
+        /// </summary>
+        [PersistMember(1)]
+        public DateTime Time { get; private set; }
+        
+        /// <summary>
+        /// The author who created the contact.
+        /// </summary>
+        [PersistMember(2)]
+        public Id AuthorId { get; private set; }
+        
+        public ContactCreated() {}
+
+        public ContactCreated(Id id, DateTime time, Id author)
+        {
+            Id = id;
+            Time = time;
+            AuthorId = author;
+        }
+
+        public Id EntityId { get { return Id; } }
+    }
+}
---
+++ SocialToolBox.Crm/Contact/Projection/ContactAsEntityPage.cs
@@ -0,0 +1,31 @@
+﻿using SocialToolBox.Core;
+using SocialToolBox.Core.Database.Serialization;
+using SocialToolBox.Core.Entity.Projection;
+using SocialToolBox.Crm.Contact.Event;
+
+namespace SocialToolBox.Crm.Contact.Projection
+{
+    /// <summary>
+    /// A contact, seen as an entity page.
+    /// </summary>
+    [Persist("SocialToolBox.Crm.Contact.Projection.ContactAsEntityPage")]
+    public class ContactAsEntityPage : IEntityPage
+    {
+        /// <summary>
+        /// The name of the contact.
+        /// </summary>
+        public string Name { get; private set; }
+
+        public ContactAsEntityPage() {}
+
+        public string Title { get { return Name; } }
+
+        /// <summary>
+        /// Extends an entity page visitor to react to contact events.
+        /// </summary>
+        public static void ExtendVisitor(Visitor<IEntityPage, IEntityPage> visitor)
+        {
+            visitor.On<ContactCreated>((e,i) => new ContactAsEntityPage());
+        }
+    }
+}
