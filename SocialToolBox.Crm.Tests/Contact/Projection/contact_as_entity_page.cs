f19e2305a1bd12e4393d4aed8f42fa243aaf0d31
Victor Nicollet <victor@nicollet.net>
2013-09-23 14:34:32 +0200

    Contact as entity page : tests

---
+++ SocialToolBox.Crm.Tests/Contact/Projection/contact_as_entity_page.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NUnit.Framework;
+using SocialToolBox.Core.Database;
+using SocialToolBox.Core.Entity;
+using SocialToolBox.Core.Mocks.Database;
+using SocialToolBox.Crm.Contact;
+using SocialToolBox.Crm.Contact.Event;
+using SocialToolBox.Crm.Contact.Projection;
+
+namespace SocialToolBox.Crm.Tests.Contact.Projection
+{
+    [TestFixture]
+    public class contact_as_entity_page
+    {
+        public ContactModule Contacts;
+        public EntityModule Entities;
+        public ProjectionEngine Projections;
+
+        public Id IdA = Id.Parse("aaaaaaaaaaa");
+        public Id IdUser = Id.Parse("user123user");
+
+        [SetUp]
+        public void SetUp()
+        {
+            var driver = new DatabaseDriver();
+            Contacts = new ContactModule(driver);
+            Entities = new EntityModule(driver);
+            Contacts.RegisterContactsAsEntities(Entities);
+            Projections = driver.Projections;
+            
+            Entities.Compile();
+        }
+        [Test]
+        public void initially_empty()
+        {
+            Assert.IsNull(Entities.Pages.Get(IdA).Result);
+        }
+
+        [Test]
+        public void create_provides_title()
+        {
+            Contacts.Stream.AddEvent(new ContactCreated(IdA, DateTime.Parse("2011/05/14"), IdUser));
+            Projections.Run();
+
+            Assert.AreEqual(typeof(ContactAsEntityPage), Entities.Pages.Get(IdA).Result.GetType());
+            Assert.IsNull(Entities.Pages.Get(IdA).Result.Title);
+        }
+
+        [Test]
+        public void update_changes_title()
+        {
+            Contacts.Stream.AddEvent(new ContactCreated(IdA, DateTime.Parse("2011/05/14"), IdUser));
+            Contacts.Stream.AddEvent(new ContactNameUpdated(IdA, DateTime.Parse("2011/05/14"), IdUser, "Victor", "Nicollet"));
+            Projections.Run();
+
+            Assert.AreEqual("Victor Nicollet", Entities.Pages.Get(IdA).Result.Title);
+        }
+
+        [Test]
+        public void delete_removes_page()
+        {
+            Contacts.Stream.AddEvent(new ContactCreated(IdA, DateTime.Parse("2011/05/14"), IdUser));
+            Contacts.Stream.AddEvent(new ContactDeleted(IdA, DateTime.Parse("2011/05/14"), IdUser));
+            Projections.Run();
+
+            Assert.IsNull(Entities.Pages.Get(IdA).Result);
+        }
+
+        [Test]
+        public void missing_without_create()
+        {
+            Contacts.Stream.AddEvent(new ContactNameUpdated(IdA, DateTime.Parse("2011/05/14"), IdUser, "Victor", "Nicollet"));
+            Projections.Run();
+
+            Assert.IsNull(Entities.Pages.Get(IdA).Result);
+        }
+    }
+}
--- SocialToolBox.Crm/Contact/Projection/ContactAsEntityPage.cs
+++ SocialToolBox.Crm/Contact/Projection/ContactAsEntityPage.cs
@@ -14,6 +14,7 @@ namespace SocialToolBox.Crm.Contact.Projection
         /// <summary>
         /// The name of the contact.
         /// </summary>
+        [PersistMember(0)]
         public string Name { get; private set; }
 
         public ContactAsEntityPage() {}
