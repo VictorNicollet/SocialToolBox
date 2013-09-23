bf0eff1c11f6f490fa39429cabda527923d3f92b
Victor Nicollet <victor@nicollet.net>
2013-09-23 11:57:43 +0200

    Visiting extractor

---
+++ SocialToolBox.Core.Tests/visiting_extractor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using SocialToolBox.Core.Database;
+using SocialToolBox.Core.Mocks.Database.Events;
+
+namespace SocialToolBox.Core.Tests
+{
+    [TestFixture]
+    public class visiting_extractor
+    {
+        [Test]
+        public void find_event()
+        {
+            var visitor = new VisitingExtractor<string>();
+            visitor.On<MockAccountCreated>(e => e.Name);
+
+            var ev = new MockAccountCreated(Id.Parse("aaaaaaaaaaa"), "Test", DateTime.Parse("2012/05/21"));
+
+            var m = visitor.Visit(ev);
+
+            Assert.AreEqual("Test", m);
+        }
+
+        [Test]
+        public void find_event_by_interface()
+        {
+            var visitor = new VisitingExtractor<DateTime>();
+            visitor.On<IMockEvent>(e => e.Time);
+
+            var date = DateTime.Parse("2012/05/21");
+            var ev = new MockAccountDeleted(Id.Parse("aaaaaaaaaaa"), date);
+
+            var date2 = visitor.Visit(ev);
+
+            Assert.AreEqual(date, date2);
+        }
+    }
+}
---
+++ SocialToolBox.Core/VisitingExtractor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocialToolBox.Core
+{
+    /// <summary>
+    /// Acts as a visitor, but does not expect any input.
+    /// </summary>
+    public class VisitingExtractor<TOutput>
+    {
+        /// <summary>
+        /// A visitor action. These are stored for each type.
+        /// </summary>
+        private delegate TOutput VisitorAction(object e);
+
+        /// <summary>
+        /// The visitor action registered for each type.
+        /// </summary>
+        private readonly Dictionary<Type,VisitorAction> _funcs = 
+            new Dictionary<Type,VisitorAction>();
+
+        /// <summary>
+        /// The action performed if no other action was found. 
+        /// </summary>
+        private VisitorAction _defaultAction;
+
+        /// <summary>
+        /// Registers a new event handler for the specified event type.
+        /// </summary>
+        /// <remarks>
+        /// For class types, only instances of that class (not derived 
+        /// classes) are affected. For interfaces, all classes that
+        /// implement that interface are affected, unless they have
+        /// their own binding.
+        /// </remarks>
+        public void On<TEvent>(Func<TEvent, TOutput> f)
+        {
+            try
+            {
+                _funcs.Add(typeof (TEvent), e => f((TEvent) e));
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException("Could not register visitor.", e);
+            }
+        }
+
+        /// <summary>
+        /// Specifies a default action for the visitor.
+        /// </summary>
+        public void SetDefaultAction(Func<object, TOutput> f)
+        {
+            _defaultAction = e => f(e);
+        }
+
+        /// <summary>
+        /// Finds the visitor action for a specified type.
+        /// </summary>
+        private void FindForType(Type t, out VisitorAction action)
+        {
+            if (_funcs.TryGetValue(t, out action)) return;
+
+            foreach (var i in t.GetInterfaces())
+                if (_funcs.TryGetValue(i, out action)) return;
+
+            action = _defaultAction;
+        }
+
+        /// <summary>
+        /// Visits the object, determining the required action based on its type.
+        /// Action receives input data and object, its return value is returned. 
+        /// </summary>
+        public TOutput Visit(object e)
+        {
+            if (e == null)
+                throw new ArgumentNullException("e");
+
+            VisitorAction action;
+            FindForType(e.GetType(), out action);
+            
+            if (action == null)
+                throw new ArgumentException(
+                    string.Format("No visitor action for type {0}", e.GetType()),
+                    "e");
+                    
+            return action(e);    
+        }
+    }
+}
