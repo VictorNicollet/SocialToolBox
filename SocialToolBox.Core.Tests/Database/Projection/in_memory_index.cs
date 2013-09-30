74889da2c66108204f577513833cf75240eff8b5
Victor Nicollet <victor@nicollet.net>
2013-09-30 12:32:24 +0200

    Start testing in-memory index

---
+++ SocialToolBox.Core.Tests/Database/Projection/in_memory_index.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using SocialToolBox.Core.Database;
+using SocialToolBox.Core.Database.Index;
+using SocialToolBox.Core.Mocks.Database;
+using SocialToolBox.Core.Mocks.Database.Projections;
+
+namespace SocialToolBox.Core.Tests.Database.Projection
+{
+    [TestFixture]
+    public class in_memory_index
+    {
+        public readonly Id IdA = Id.Parse("aaaaaaaaaaa");
+        public readonly Id IdB = Id.Parse("bbbbbbbbbbb");
+        public InMemoryIndex<StringKey, StringKey> Index;
+        public IProjectCursor Cursor;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var driver = new DatabaseDriver();
+            Index = new InMemoryIndex<StringKey, StringKey>();
+            Cursor = driver.OpenProjectionCursor();
+        }
+
+        public StringKey K(string k) { return new StringKey(k); }
+
+        [Test]
+        public void default_is_empty()
+        {
+            Assert.AreEqual(0, Index.Count(K("A"), Cursor).Result);
+        }
+    }
+}
