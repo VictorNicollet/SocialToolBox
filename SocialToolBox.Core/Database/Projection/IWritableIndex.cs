44a8b938014b54a6b81cc4fd529ee0ac74698c3a
Victor Nicollet <victor@nicollet.net>
2013-09-30 11:23:17 +0200

    Index write interface

---
+++ SocialToolBox.Core/Database/Projection/IWritableIndex.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SocialToolBox.Core.Database.Projection
+{
+    /// <summary>
+    /// Provides write-access to an index.
+    /// </summary>
+    public interface IWritableIndex<in TSet, TSort> : IIndex<TSet, TSort>
+        where TSet : class where TSort : class
+    {
+        /// <summary>
+        /// Delete all index bindings for the specified identifier.
+        /// </summary>
+        Task Delete(Id id, IProjectCursor c);
+
+        /// <summary>
+        /// Deletes a specified set for the specified identifier.
+        /// </summary>
+        Task DeleteSet(Id id, TSet set, IProjectCursor c);
+
+        /// <summary>
+        /// Adds a binding for a given identifier.
+        /// </summary>
+        Task Add(Id id, TSet set, TSort sort, IProjectCursor c);
+
+        /// <summary>
+        /// Adds several bindings for a given identifier.
+        /// </summary>
+        Task Add(Id id, IEnumerable<IPair<TSet, TSort>> bindings, IProjectCursor c);
+    }
+}
