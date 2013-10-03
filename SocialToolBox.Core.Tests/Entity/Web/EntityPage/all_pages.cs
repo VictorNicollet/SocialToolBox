64c24f1c6e231487ee9bc6a862c388dcb900ad60
Victor Nicollet <victor@nicollet.net>
2013-10-03 22:44:09 +0200

    Tests for all pages.

---
+++ SocialToolBox.Core.Tests/Entity/Web/EntityPage/all_pages.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using SocialToolBox.Core.Present;
+using SocialToolBox.Core.Web.Args;
+using SocialToolBox.Core.Web.Response;
+
+namespace SocialToolBox.Core.Tests.Entity.Web.EntityPage
+{
+    [TestFixture]
+    public class all_pages : MockFixture
+    {
+        [Test]
+        public void get_page_1()
+        {
+            var response = (WebResponsePage) Facet.All.Query(new PageArgs()).Run();
+            var page = response.Page as ColumnPage;
+            Assert.IsNotNull(page);
+            Assert.AreEqual(1, page.Columns.Length);
+
+            var list = page.Columns[0].First() as ListVertical;
+            Assert.IsNotNull(list);
+            Assert.AreEqual(2, list.Items.Count);
+        }
+
+        [Test]
+        public void get_page_2()
+        {
+            var response = (WebResponsePage)Facet.All.Query(new PageArgs(1)).Run();
+            var page = response.Page as ColumnPage;
+            Assert.IsNotNull(page);
+            Assert.AreEqual(1, page.Columns.Length);
+
+            var list = page.Columns[0].First() as ListVertical;
+            Assert.IsNotNull(list);
+            Assert.AreEqual(0, list.Items.Count);
+        }
+    }
+}
