221d818f1efc1424f0383f6f0bcfd3ab8424ed4a
Victor Nicollet <victor@nicollet.net>
2013-10-07 11:45:33 +0200

    Page created event persistence

--- SocialToolBox.Cms/Page/Event/PageCreated.cs
+++ SocialToolBox.Cms/Page/Event/PageCreated.cs
@@ -10,8 +10,13 @@ namespace SocialToolBox.Cms.Page.Event
     [Persist("SocialToolBox.Cms.Page.Events.PageCreated")]
     public class PageCreated : IPageEvent
     {
+        [PersistMember(0)]
         public Id Id { get; private set; }
+        
+        [PersistMember(1)]
         public DateTime Time { get; private set; }
+        
+        [PersistMember(2)]
         public Id AuthorId { get; private set; }
 
         public PageCreated() {}
