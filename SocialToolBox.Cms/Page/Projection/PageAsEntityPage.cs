d36ad19d6fbf4a6382a49a3a1038a443c1d8a39e
Victor Nicollet <victor@nicollet.net>
2013-10-07 12:00:43 +0200

    Page as entity page

---
+++ SocialToolBox.Cms/Page/Projection/PageAsEntityPage.cs
@@ -0,0 +1,34 @@
+﻿using SocialToolBox.Cms.Page.Event;
+using SocialToolBox.Core;
+using SocialToolBox.Core.Database.Serialization;
+using SocialToolBox.Core.Entity.Projection;
+
+namespace SocialToolBox.Cms.Page.Projection
+{
+    /// <summary>
+    /// A page, seen as an entity page.
+    /// </summary>
+    [Persist("SocialToolBox.Cms.Page.Projection.PageAsEntityPage")]
+    public sealed class PageAsEntityPage : IEntityPage
+    {
+        public PageAsEntityPage() {}
+
+        public string Title { get; private set; }
+
+        /// <summary>
+        /// Extends an entity page visitor to react to contacte events.
+        /// </summary>
+        public static void ExtendVisitor(Visitor<IEntityPage, IEntityPage> visitor)
+        {
+            visitor.On<PageCreated>((e,i) => new PageAsEntityPage());
+            visitor.On<PageDeleted>((e,i) => null);
+
+            visitor.On<PageTitleUpdated>((e, i) =>
+            {
+                var old = i == null ? null : i as PageAsEntityPage;
+                if (old != null) old.Title = e.Title;
+                return i;
+            });
+        }
+    }
+}
