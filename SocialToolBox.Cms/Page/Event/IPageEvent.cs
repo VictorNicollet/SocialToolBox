ad7c66ab74b96cb131a2ac1a85137781c591321c
Victor Nicollet <victor@nicollet.net>
2013-10-07 11:42:56 +0200

    CMS project

---
+++ SocialToolBox.Cms/Page/Event/IPageEvent.cs
@@ -0,0 +1,12 @@
+﻿using SocialToolBox.Core.Database.Event;
+using SocialToolBox.Core.User.Event;
+
+namespace SocialToolBox.Cms.Page
+{
+    /// <summary>
+    /// An individual event about a CMS page.
+    /// </summary>
+    public interface IPageEvent : IEventWithId, IEventWithTime, IEventByUser
+    {
+    }
+}
---
+++ SocialToolBox.Cms/Page/Event/PageCreated.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SocialToolBox.Core.Database;
+using SocialToolBox.Core.Database.Serialization;
+
+namespace SocialToolBox.Cms.Page.Event
+{
+    /// <summary>
+    /// A new page is created.
+    /// </summary>
+    [Persist("SocialToolBox.Cms.Page.Events.PageCreated")]
+    public class PageCreated : IPageEvent
+    {
+        public Id Id { get; private set; }
+        public DateTime Time { get; private set; }
+        public Id AuthorId { get; private set; }
+
+        public PageCreated() {}
+
+        public PageCreated(Id id, DateTime time, Id author)
+        {
+            AuthorId = author;
+            Id = id;
+            Time = time;
+        }
+    }
+}
