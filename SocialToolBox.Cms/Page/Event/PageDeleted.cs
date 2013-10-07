915532fcfc134f8a8bf25328cc04f39e8d399c72
Victor Nicollet <victor@nicollet.net>
2013-10-07 11:51:37 +0200

    Page deleted event

---
+++ SocialToolBox.Cms/Page/Event/PageDeleted.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SocialToolBox.Core.Database;
+using SocialToolBox.Core.Database.Serialization;
+
+namespace SocialToolBox.Cms.Page.Event
+{
+    /// <summary>
+    /// A page was deleted.
+    /// </summary>
+    [Persist("SocialToolBox.Cms.Page.Event.PageDeleted")]
+    public class PageDeleted : IPageEvent
+    {
+        [PersistMember(0)]
+        public Id Id { get; private set; }
+        
+        [PersistMember(1)]
+        public DateTime Time { get; private set; }
+        
+        [PersistMember(2)]
+        public Id AuthorId { get; private set; }
+    
+        public PageDeleted() {}
+
+        public PageDeleted(Id id, DateTime time, Id author)
+        {
+            Id = id;
+            Time = time;
+            AuthorId = author;
+        }
+    }
+}
