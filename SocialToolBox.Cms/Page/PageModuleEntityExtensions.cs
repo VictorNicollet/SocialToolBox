1a0c95ddee79fd44040467c56a30b09ecf5fc53f
Victor Nicollet <victor@nicollet.net>
2013-10-07 12:29:02 +0200

    Page entity extensions

---
+++ SocialToolBox.Cms/Page/PageModuleEntityExtensions.cs
@@ -0,0 +1,22 @@
+﻿using SocialToolBox.Cms.Page.Projection;
+using SocialToolBox.Core.Entity;
+
+namespace SocialToolBox.Cms.Page
+{
+    /// <summary>
+    /// Extensions involved in making the page module connect to the
+    /// entity module.
+    /// </summary>
+    public static class PageModuleEntityExtensions
+    {        
+        /// <summary>
+        /// Register all pages as entities : these will appear as entity
+        /// pages, in entity searches, and so on.
+        /// </summary>
+        public static void RegisterPagesAsEntities(this PageModule pmodule, EntityModule emodule)
+        {
+            emodule.AddEventStream(pmodule.Stream);
+            PageAsEntityPage.ExtendVisitor(emodule.PageEventVisitor);
+        }
+    }
+}
