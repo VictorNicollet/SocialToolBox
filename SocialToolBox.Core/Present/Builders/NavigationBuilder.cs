3ca6513b7d7234673040235ebfdda828a6c4b274
Victor Nicollet <victor@nicollet.net>
2013-10-24 14:24:05 +0200

    Navigation implementation

---
+++ SocialToolBox.Core/Present/Builders/NavigationBuilder.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using SocialToolBox.Core.Web;
+
+namespace SocialToolBox.Core.Present.Builders
+{
+    // TODO: test
+
+    /// <summary>
+    /// Builds a <see cref="Navigation"/> object.
+    /// </summary>
+    public sealed class NavigationBuilder
+    {
+        /// <summary>
+        /// An individual link inside the navigation block.
+        /// </summary>
+        public sealed class Link
+        {
+            /// <summary>
+            /// The text to be displayed for this link.
+            /// </summary>
+            public readonly string Label;
+
+            /// <summary>
+            /// The URL activated when clicking this link.
+            /// </summary>
+            public readonly WebUrl Url;
+
+            /// <summary>
+            /// Is this the currently selected link ? 
+            /// </summary>
+            public readonly bool IsCurrent;
+
+            public Link(string label, WebUrl url, bool isCurrent = false)
+            {
+                Label = label;
+                Url = url;
+                IsCurrent = isCurrent;
+            }
+
+            public Link Clone(bool isCurrent = false)
+            {
+                return new Link(Label, Url, isCurrent);
+            }
+        }
+
+        /// <summary>
+        /// Primary links.
+        /// </summary>
+        private readonly IList<Link> _primary = new List<Link>();
+
+        /// <summary>
+        /// Secondary link.
+        /// </summary>
+        private readonly IList<Link> _secondary = new List<Link>();
+
+        /// <summary>
+        /// Primary links. If no primary links were provided, returns secondary
+        /// links instead.
+        /// </summary>
+        public IEnumerable<Link> Primary
+        {
+            get
+            {
+                return _primary.Count == 0 ? _secondary : _primary;
+            }
+        }
+
+        /// <summary>
+        /// Secondary links. If no primary links were provided, secondary links
+        /// become primary, and there are no secondary links returned here.
+        /// </summary>
+        public IEnumerable<Link> Secondary
+        {
+            get
+            {
+                return _primary.Count == 0 ? new Link[0] : _secondary;
+            }
+        }
+
+        /// <summary>
+        /// Adds a primary link to the builder, and returns the builder.
+        /// </summary>
+        public NavigationBuilder AddPrimary(Link link)
+        {
+            _primary.Add(link);
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a primary link to the builder, and returns the builder.
+        /// </summary>
+        public NavigationBuilder AddPrimary(string label, WebUrl url, bool isCurrent = false)
+        {
+            return AddPrimary(new Link(label, url, isCurrent));
+        }
+
+        /// <summary>
+        /// Adds a secondary link to the builder, and returns the builder.
+        /// </summary>
+        public NavigationBuilder AddSecondary(Link link)
+        {
+            _secondary.Add(link);
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a secondary link to the builder, and returns the builder.
+        /// </summary>
+        public NavigationBuilder AddSecondary(string label, WebUrl url, bool isCurrent = false)
+        {
+            return AddSecondary(new Link(label, url, isCurrent));
+        }
+    }
+}
--- SocialToolBox.Core/Present/Navigation.cs
+++ SocialToolBox.Core/Present/Navigation.cs
@@ -1,10 +1,69 @@
-﻿namespace SocialToolBox.Core.Present
+﻿using System.Collections.Generic;
+using System.Linq;
+using SocialToolBox.Core.Present.Builders;
+using SocialToolBox.Core.Web;
+
+namespace SocialToolBox.Core.Present
 {
     /// <summary>
     /// A navigation block. 
     /// </summary>
     public sealed class Navigation : IPageNode
     {
+        /// <summary>
+        /// An individual link inside the navigation block.
+        /// </summary>
+        public sealed class Link
+        {
+            /// <summary>
+            /// The text to be displayed for this link.
+            /// </summary>
+            public readonly string Label;
+
+            /// <summary>
+            /// The URL activated when clicking this link.
+            /// </summary>
+            public readonly WebUrl Url;
+
+            /// <summary>
+            /// Is this the currently selected link ? 
+            /// </summary>
+            public readonly bool IsCurrent;
+
+            public Link(NavigationBuilder.Link link)
+            {
+                Label = link.Label;
+                Url = link.Url;
+                IsCurrent = link.IsCurrent;
+            }
+        }
+
+        /// <summary>
+        /// The primary links of this navigation block. Recommended
+        /// display: top or left.
+        /// </summary>
+        public readonly IReadOnlyList<Link> PrimaryLinks;
+
+        /// <summary>
+        /// The secondary links of this navigation block. Recommended 
+        /// display: bottom or right.
+        /// </summary>
+        public readonly IReadOnlyList<Link> SecondaryLinks;
+
+        /// <summary>
+        /// Start building a new item.
+        /// </summary>
+        public static NavigationBuilder Build()
+        {
+            return new NavigationBuilder();
+        }
+
+        public Navigation(NavigationBuilder builder)
+        {
+            PrimaryLinks = builder.Primary.Select(link => new Link(link)).ToList();
+            SecondaryLinks = builder.Secondary.Select(link => new Link(link)).ToList();
+        }
+
         public void RenderWith(INodeRenderer visitor, HtmlOutput output)
         {
             visitor.Render(this, output);
