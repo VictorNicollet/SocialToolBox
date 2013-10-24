29380dc76fee7086e4452df16ed76f680c0b39c7
Victor Nicollet <victor@nicollet.net>
2013-10-24 12:33:54 +0200

    Test rich user content

---
+++ SocialToolBox.Core.Tests/Present/rich_user_content.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using SocialToolBox.Core.Present;
+
+namespace SocialToolBox.Core.Tests.Present
+{
+    [TestFixture]
+    public sealed class rich_user_content
+    {
+        [Test]
+        public void has_content()
+        {
+            var h = HtmlString.Verbatim("abc");
+            var ruc = new RichUserContent(h);
+            Assert.AreSame(h, ruc.Html);
+        }
+    }
+}
