004bfef34327b9ce7d63f8fd40700b985700acb7
Victor Nicollet <victor@nicollet.net>
2013-10-24 11:27:05 +0200

    Pair equality and tests

---
+++ SocialToolBox.Core.Tests/pair.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace SocialToolBox.Core.Tests
+{
+    [TestFixture]
+    public sealed class pair
+    {
+        [Test]
+        public void strings()
+        {
+            var p = Pair.Make("a", "b");
+            Assert.AreEqual("a", p.First);
+            Assert.AreEqual("b", p.Second);
+        }
+
+        [Test]
+        public void equality()
+        {
+            var p1 = Pair.Make("a", "bb");
+            var p2 = Pair.Make("a", "b" + "b");
+            Assert.AreEqual(p1, p2);
+        }
+    }
+}
--- SocialToolBox.Core/Pair.cs
+++ SocialToolBox.Core/Pair.cs
@@ -1,16 +1,14 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace SocialToolBox.Core
+﻿namespace SocialToolBox.Core
 {
     /// <summary>
     /// A pair of values.
     /// </summary>
-    public class Pair
+    public static class Pair
     {
         /// <summary>
         /// Internal implementation.
         /// </summary>
-        private class Impl<TA, TB> : IPair<TA, TB>
+        private sealed class Impl<TA, TB> : IPair<TA, TB>
         {
             public TA First { get; private set; }
             public TB Second { get; private set; }
@@ -19,6 +17,32 @@ namespace SocialToolBox.Core
             {
                 First = first;
                 Second = second;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj == null) return false;
+
+                var asPair = obj as IPair<TA, TB>;
+                if (asPair == null) return false;
+
+                // ReSharper disable CompareNonConstrainedGenericWithNull
+                if (First == null && asPair.First != null) return false;
+                if (Second == null && asPair.Second != null) return false;
+
+                return ((First == null || First.Equals(asPair.First))
+                    && (Second == null || Second.Equals(asPair.Second)));
+                // ReSharper restore CompareNonConstrainedGenericWithNull
+            }
+
+            public override int GetHashCode()
+            {
+                // ReSharper disable CompareNonConstrainedGenericWithNull
+                var hasha = First == null ? 0 : First.GetHashCode();
+                var hashb = Second == null ? 0 : Second.GetHashCode();
+                // ReSharper restore CompareNonConstrainedGenericWithNull
+
+                return hasha ^ hashb;
             }
         }
 
