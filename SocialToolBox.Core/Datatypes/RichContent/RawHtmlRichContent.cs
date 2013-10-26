c1d9a8d6a1a70a145abe578a0a4c3e3a1455de1b
Victor Nicollet <victor@nicollet.net>
2013-10-26 11:37:43 +0200

    Rich content : raw HTML

---
+++ SocialToolBox.Core/Datatypes/RichContent/RawHtmlRichContent.cs
@@ -0,0 +1,16 @@
+﻿using SocialToolBox.Core.Present;
+
+namespace SocialToolBox.Core.Datatypes.RichContent
+{
+    /// <summary>
+    /// Rich content typed in as raw HTML. May require sanitization
+    /// before being output.
+    /// </summary>
+    public class RawHtmlRichContent : IRichContent
+    {
+        public HtmlString ToHtml()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
