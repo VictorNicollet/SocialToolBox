df8fc815ceaf64017ee45ccdcdf0a937830db72e
Victor Nicollet <victor@nicollet.net>
2013-10-26 11:27:38 +0200

    Discussion event : discussion was created

---
+++ SocialToolBox.Core/Discussion/Event/DiscussionCreated.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SocialToolBox.Core.Database;
+using SocialToolBox.Core.Database.Event;
+using SocialToolBox.Core.Database.Serialization;
+using SocialToolBox.Core.User.Event;
+
+namespace SocialToolBox.Core.Discussion.Event
+{
+    /// <summary>
+    /// A discussion was created by an author through a typical, 
+    /// manual "create discussion" process.
+    /// </summary>
+    [Persist("SocialToolBox.Core.Discussion.Event.DiscussionCreated")]
+    public sealed class DiscussionCreated : IDiscussionEvent, IEventWithTime, IEventByUser
+    {
+        [PersistMember(0)]
+        public Id Id { get; private set; }
+        
+        [PersistMember(1)]
+        public DateTime Time { get; private set; }
+        
+        [PersistMember(2)]
+        public Id AuthorId { get; private set; }
+
+        public DiscussionCreated() {}
+
+        public DiscussionCreated(Id id, DateTime time, Id author)
+        {
+            Id = id;
+            Time = time;
+            AuthorId = author;
+        }
+    }
+}
---
+++ SocialToolBox.Core/Discussion/Event/IDiscussionEvent.cs
@@ -0,0 +1,12 @@
+﻿using SocialToolBox.Core.Database.Event;
+
+namespace SocialToolBox.Core.Discussion.Event
+{
+    /// <summary>
+    /// An event that is related to a discussion. Since 
+    /// discussions are entities, each discussion 
+    /// </summary>
+    public interface IDiscussionEvent : IEventWithId
+    {
+    }
+}
