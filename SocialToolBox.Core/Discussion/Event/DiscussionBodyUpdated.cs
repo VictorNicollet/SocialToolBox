25fa58bc27fce48c7768c6e5a06098e09ead34e4
Victor Nicollet <victor@nicollet.net>
2013-10-26 11:33:09 +0200

    Discussion event : discussion body updated

---
+++ SocialToolBox.Core/Discussion/Event/DiscussionBodyUpdated.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SocialToolBox.Core.Database;
+using SocialToolBox.Core.Database.Event;
+using SocialToolBox.Core.Database.Serialization;
+using SocialToolBox.Core.User.Event;
+
+namespace SocialToolBox.Core.Discussion.Event
+{
+    [Persist("SocialToolBox.Core.Discussion.Event.DiscussionBodyUpdated")]
+    public sealed class DiscussionBodyUpdated : IDiscussionEvent, IEventWithTime, IEventByUser
+    {
+        [PersistMember(0)]
+        public Id Id { get; private set; }
+        
+        [PersistMember(1)]
+        public DateTime Time { get; private set; }
+        
+        [PersistMember(2)]
+        public Id AuthorId { get; private set; }
+
+        [PersistMember(3)]
+        public string Body { get; private set; }
+
+        public DiscussionBodyUpdated() {}
+
+        public DiscussionBodyUpdated(Id id, DateTime time, Id author, string body)
+        {
+            Id = id;
+            Time = time;
+            AuthorId = author;
+            Body = body;
+        }
+    }
+}
