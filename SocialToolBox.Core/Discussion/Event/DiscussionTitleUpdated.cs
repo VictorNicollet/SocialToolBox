ab2d48702789df2123383546c1b5b640a1fb3aac
Victor Nicollet <victor@nicollet.net>
2013-10-26 11:30:14 +0200

    Discussion event : discussion title updated

---
+++ SocialToolBox.Core/Discussion/Event/DiscussionTitleUpdated.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using SocialToolBox.Core.Database;
+using SocialToolBox.Core.Database.Event;
+using SocialToolBox.Core.Database.Serialization;
+using SocialToolBox.Core.User.Event;
+
+namespace SocialToolBox.Core.Discussion.Event
+{
+    /// <summary>
+    /// The title of a discussion was set to a new value.
+    /// </summary>
+    [Persist("SocialToolBox.Core.Discussion.Event.DiscussionTitleUpdated")]
+    public sealed class DiscussionTitleUpdated : IDiscussionEvent, IEventByUser, IEventWithTime
+    {
+        [PersistMember(0)]
+        public Id Id { get; private set; }
+        
+        [PersistMember(1)]
+        public DateTime Time { get; private set; }
+        
+        [PersistMember(2)]
+        public Id AuthorId { get; private set; }
+        
+        [PersistMember(3)]
+        public string Title { get; private set; }
+
+        public DiscussionTitleUpdated() {}
+
+        public DiscussionTitleUpdated(Id id, DateTime time, Id author, string title)
+        {
+            Id = id;
+            Time = time;
+            AuthorId = author;
+            Title = title;
+        }
+    }
+}
