c38b9a06cb5a63b348ba059bde296086c68140d1
Victor Nicollet <victor@nicollet.net>
2013-10-26 11:35:07 +0200

    Rich content

---
+++ SocialToolBox.Core/Datatypes/RichContent/IRichContent.cs
@@ -0,0 +1,17 @@
+﻿using SocialToolBox.Core.Present;
+
+namespace SocialToolBox.Core.Datatypes.RichContent
+{
+    /// <summary>
+    /// Rich content is content entered by an end user that can be turned
+    /// into HTML. This could be actual HTML, or Markdown, or another format
+    /// that can be compiled to HTML.
+    /// </summary>
+    public interface IRichContent
+    {
+        /// <summary>
+        /// Compile the rich content to an HTML string.
+        /// </summary>
+        HtmlString ToHtml();
+    }
+}
